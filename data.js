c131f56cd1e32b9547929013e9321e32da1fbd64
Daniel Bennington <dbennington125@gmail.com>
2020-04-06 14:28:42 -0700

    html made, button added, data added, home page added

---
+++ data.js
@@ -0,0 +1,9 @@
+const workoutHour = {
+    id: 'hour workout',
+    name: 'One hour session',
+    image: 'hour-workout.jpeg', //image: ./asset/hoursesion? I read the directions and I don't think we've done this.
+    description:'A full hour of one on one coacing from our highly trained staff',
+    category: 'service',
+    price:  70.00,
+    
+};
