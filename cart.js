cbcbd5b797f86d7e0f3276c36db3784f3835035b
Daniel Bennington <dbennington125@gmail.com>
2020-04-07 14:56:10 -0700

    testing function findById

--- cart.js
+++ cart.js
@@ -1,1 +1,13 @@
-const 
+const Protein = {
+    id: 'protein',
+    quanity: 2,
+};
+const PreWorkOut = {
+    id: 'preWorkout',
+    quanity: 1,
+};
+const cartItems = [
+    Protein,
+    PreWorkOut
+];
+export default cartItems;
--- test/findById.test.js
+++ test/findById.test.js
@@ -1,12 +1,12 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-
+//import findById from './ultils.js';
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+ 
     //Act 
     // Call the function you're testing and set the result to a const
 
