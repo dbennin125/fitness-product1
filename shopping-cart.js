3b09a0cb2994212038d372a5f2c265656c486233
Daniel Bennington <dbennington125@gmail.com>
2020-04-08 10:09:35 -0700

    table is populating

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,26 +1,26 @@
 import cartItems from './cart.js'; //importing the data from data array
-import itemsSold from './data.js'; //my products
+import productsSold from './data.js'; //my products
 import { findById } from './utils.js';
 //import { calcLineTotal } from './utils.js';
 import makeTable from './CartDOMRender.js';//importing the DOM render
 
 
 //creating our name for container and getting element from HTML
-const tableCountainer = document.getElementById('table');
+const myTbody = document.querySelector('tbody');
 
 
 //creating the button
-const buyButton = document.getElementById('buyItem');
+//const buyButton = document.getElementById('buyItem');
 //going through the cart
 for (let i = 0; i < cartItems.length; i++) {
     //going through cart array to find info
-    const boughtItem = cartItems [i];
+    const boughtItem = cartItems[i];
     //going through products array to get a match
-    const boughtProduct = findById(itemsSold, boughtItem);
+    const boughtProduct = findById(productsSold, boughtItem.id);
     //making a table with the match
-    const table = makeTable(boughtProduct);
-
-    tableCountainer.appendChild(table);
+    const result = makeTable(boughtProduct, boughtItem);
+    
+    
+    myTbody.appendChild(result);
 }
 
-buyButton.value = '';
